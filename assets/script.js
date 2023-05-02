18e544b6a46bc5f7bb88cc3c0594fefb3ff5455d
VincenzoMinardi <vincenzo.minardi.mv@gmail.com>
2023-05-02 15:55:39 +0200

    .

--- assets/script.js
+++ assets/script.js
@@ -31,3 +31,87 @@ const arrayImage = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+const containerHighlighted = document.querySelector('.highlighted');
+const containerThumbs = document.querySelector('.thumbs');
+
+
+for (let i = 0; i < arrayImage.length; i++) {
+	containerHighlighted.innerHTML += `
+    
+        <img src="${arrayImage[i].image}" alt="" class="${i == 0 ? 'active' : ''}">
+        <div class="title">${arrayImage[i].title}</div> 
+        <div class="text"> ${arrayImage[i].text}</div>`;
+
+	containerThumbs.innerHTML += `<img src="${arrayImage[i].image}" alt="" class="${i == 0 ? 'active' : ''}">`;
+}
+
+
+// selezionimo le immagini nell'html
+const title = document.querySelector('.title');
+const text = document.querySelector('.text');
+const listHighlighted = document.querySelectorAll('.highlighted img');
+// selezioniamo le miniature
+const listThumbs = document.querySelectorAll('.thumbs img');
+// selezioniamo i bottoni
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+
+
+// definito una variabile che rappresenta lo stato attuale del carosello
+// cioe' l'indice dell'immagine attiva
+let activeIndex = 0;
+
+btnNext.addEventListener('click',
+	function() {
+		// dall'immagine attiva tolgo la classe active
+       
+		listHighlighted[activeIndex].classList.remove('active');
+		listThumbs[activeIndex].classList.remove('active');
+		// settiamo il nuovo valore di active index
+		activeIndex++;
+		if (activeIndex >= listHighlighted.length) {
+			activeIndex = 0;
+		}
+		// alla nuova immagine attiva aggiungiamo la classe active
+       
+		listHighlighted[activeIndex].classList.add('active');
+		listThumbs[activeIndex].classList.add('active');
+	}
+);
+
+btnPrev.addEventListener('click',
+	function() {
+		// dall'immagine attiva tolgo la classe active
+        
+		listHighlighted[activeIndex].classList.remove('active');
+		listThumbs[activeIndex].classList.remove('active');
+		// settiamo il nuovo valore di active index
+		activeIndex--;
+		if (activeIndex < 0) {
+			activeIndex = listHighlighted.length - 1;
+		}
+		// alla nuova immagine attiva aggiungiamo la classe active
+       
+		listHighlighted[activeIndex].classList.add('active');
+		listThumbs[activeIndex].classList.add('active');
+	}
+);
+
+// ciclo per aggiungere gli event listeners alle miniature
+for (let i = 0; i < listThumbs.length; i++) {
+	listThumbs[i].addEventListener('click',
+		function() {
+			console.log('cliccata la miniature in posizione ' + i)
+          
+			listHighlighted[activeIndex].classList.remove('active');
+			listThumbs[activeIndex].classList.remove('active');
+			activeIndex = i;
+            listHighlighted[activeIndex].classList.add('active');
+			listThumbs[activeIndex].classList.add('active');
+		}
+	)
+}
+
+
+
